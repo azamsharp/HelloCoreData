0ca09f6d760e3b67b1d195f23202d8a9030101bc
Mohammad Azam <azamsharp@gmail.com>
2021-02-14 18:29:10 -0600

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by Mohammad Azam on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
